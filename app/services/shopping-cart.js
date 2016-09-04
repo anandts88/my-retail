533285b1e7af2b6c27c0d884c0f472e1c75a93b3
Anand Sakthivel <anandts88@gmail.com>
2016-09-04 17:11:09 -0400

    Comments added

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -26,11 +26,15 @@ export default Service.extend({
 
   add(item, quantity, online=true) {
     const items = get(this, 'items');
+    // Find items in the cart based on supplied arguments. `item.id` and `online`.
     const find = items.find((_item) => get(_item, 'item.id') === get(item, 'id') && get(_item, 'online') === online);
 
+    // If the item is not available in the cart.
     if (isEmpty(find)) {
+      // Add the item into cart.
       items.pushObject({ online, item, quantity });
     } else {
+      // If item is already available in cart then update quantity alone.
       set(find, 'quantity', get(find, 'quantity') + quantity);
     }
   }
