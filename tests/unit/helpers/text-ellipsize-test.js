59d66a013b7b70ec6e5a980acd21ead476d5b7cc
Anand Sakthivel <anandts88@gmail.com>
2016-09-04 17:28:57 -0400

    Unit test added for text ellipsize helper

--- tests/unit/helpers/text-ellipsize-test.js
+++ tests/unit/helpers/text-ellipsize-test.js
@@ -5,6 +5,7 @@ module('Unit | Helper | text ellipsize');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = textEllipsize([42]);
-  assert.ok(result);
+  let result = textEllipsize(["Hello"], { chars: 2 });
+
+  assert.equal(result, "He ...");
 });
